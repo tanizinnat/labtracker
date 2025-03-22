a7f8ce1954e35148d235fd455c5a5b6405cbf29d
tanizinnat <tani15-5315@diu.edu.bd>
2025-03-23 00:07:35 +0600

    problam2

---
+++ lib/problam2.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WelcomeApp());
+}
+
+class WelcomeApp extends StatelessWidget {
+  const WelcomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  String message = 'Hi, Welcome!';
+
+  
+  void updateMessage() {
+    setState(() {
+      message = 'Button Pressed!';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Button App')),
+        backgroundColor: Colors.teal, 
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              message,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.redAccent, 
+                fontFamily: 'DMSans',
+              ),
+            ),
+            const SizedBox(height: 20), 
+            ElevatedButton(
+              onPressed: updateMessage,
+              child: const Text('Tap Me'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
